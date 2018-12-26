71593eda9f12d21a4b364098c95a75590f507fa8
el_viejo27 <el_viejo27@hotmail.com>
2018-12-26 20:39:34 -0300

    añadiendo cositas al app.js, añadiendo mejoras a server.js

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('abrieron la página papá!!!');
+  next();
+});
 
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({message: "funciona bien y esto es un JSON papá!!!"});
+  next();
+});
 
 module.exports = app;
